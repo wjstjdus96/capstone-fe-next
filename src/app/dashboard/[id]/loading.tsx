f372fecc7b13f3a210a7ca7b8dd369d8b679a260
seoyeon <wjstjdus96@naver.com>
2023-09-06 00:04:59 +0900

    feat: Dashboard 로딩 페이지 생성

---
+++ src/app/dashboard/[id]/loading.tsx
@@ -0,0 +1,9 @@
+export default function loading() {
+  return (
+    <div className="w-full h-full relative">
+      <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 font-bold text-4xl">
+        Loading...
+      </div>
+    </div>
+  );
+}
