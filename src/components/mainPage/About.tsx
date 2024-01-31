10f4855bc1730bdaeb0d1a02b8f6c1c2a82e9493
seoyeon <wjstjdus96@naver.com>
2024-01-31 17:39:27 +0900

    feat: About 페이지 UI 구현

--- src/components/mainPage/About.tsx
+++ src/components/mainPage/About.tsx
@@ -1,3 +1,32 @@
+const about_card_list = [
+  { cardBody: "영상에 대한 자세한 피드백을 받고 싶으신가요?" },
+  { cardBody: "구독자가 많아 댓글을 하나하나 보기 힘드신가요?" },
+  { cardBody: "댓글의 실시간 분위기를 보고 싶으신가요?" },
+  { cardBody: "비슷한 다른 영상의 분석 결과를 얻고 싶으신가요?" },
+];
+
+const about_detail_description =
+  "TubeAna는 당신의 유튜브 영상을 더 멋있게 만들 수 있도록 도와줍니다. 다양한 분석 결과를 얻어보세요!";
+
 export default function About() {
-  return <div className="h-[100vh] bg-[#FCBF49]"></div>;
+  return (
+    <div className="h-[95vh] bg-[#FCBF49] p-[2rem]">
+      <div className="text-white text-5xl font-bold">어떤 서비스인가요?</div>
+      <div className="flex flex-col gap-[0.5rem] justify-center items-center py-[3.5rem]">
+        {about_card_list.map((card, index) => (
+          <div className=" w-[28rem] relative h-[6rem]">
+            <div
+              className={`bg-white p-[2.3rem] rounded-3xl  h-[6rem] w-[28rem] flex justify-center items-center absolute ${
+                index % 2 == 0 ? "right-[12rem]" : "left-[12rem]"
+              }`}
+            >
+              {card.cardBody}
+            </div>
+          </div>
+        ))}
+        <div></div>
+      </div>
+      <div className="text-center text-xl">{about_detail_description}</div>
+    </div>
+  );
 }
--- src/components/mainPage/Intro.tsx
+++ src/components/mainPage/Intro.tsx
@@ -8,9 +8,9 @@ export default function Intro() {
         <div className="text-4xl flex flex-col gap-[1.2rem]">
           <div>유튜브 댓글 분석을 위한</div>
           <div>새로운 플랫폼</div>
-          <div className="text-8xl mt-[2rem]">TubeAna</div>
+          <div className="text-8xl mt-[2rem] font-bold">TubeAna</div>
         </div>
-        <div className="text-xl self-end">바로 분석하러 가기</div>
+        <div className="text-xl self-end font-semibold">바로 분석하러 가기</div>
       </div>
       <Image src={bgImage} alt="메인 배경 이미지" />
     </div>
