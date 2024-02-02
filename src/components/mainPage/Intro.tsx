21a8abf76dfd627c9799684dc7fabb639018957d
seoyeon <wjstjdus96@naver.com>
2024-02-02 17:58:49 +0900

    feat: 타이핑 애니메이션 구현

--- src/components/mainPage/Intro.tsx
+++ src/components/mainPage/Intro.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import bgImage from "../../assets/bg-analysis.jpg";
+import IntroSubTitle from "./IntroSubTitle";
 
 export default function Intro({ onClickEnter }: { onClickEnter: () => void }) {
   return (
     <div className="h-[90vh] pt-[50px] w-full flex overflow-hidden">
       <div className="p-[5rem] bg-black text-white flex flex-col justify-between">
         <div className="text-4xl flex flex-col gap-[1.2rem]">
-          <div>유튜브 댓글 분석을 위한</div>
-          <div>새로운 플랫폼</div>
+          {/* <div>유튜브 댓글 분석을 위한</div>
+          <div>새로운 플랫폼</div> */}
+          <IntroSubTitle />
           <div className="text-8xl mt-[2rem] font-bold">TubeAna</div>
         </div>
         <div
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       fontFamily: {
         notoKR: ["Noto Sans KR", "sans-serif"],
       },
+      keyframes: {
+        typingCursor: {
+          from: {
+            borderRight: "2px solid white",
+          },
+          to: { borderRight: "2px solid black" },
+        },
+      },
+      animation: {
+        typingCursor: "typingCursor 1s ease-in-out 0ms 2",
+      },
     },
   },
   plugins: [],
