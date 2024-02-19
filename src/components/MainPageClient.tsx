2f05c00a0a441be306820ffb2fbe289fdccac970
seoyeon <wjstjdus96@naver.com>
2024-02-20 00:52:38 +0900

    fix: 클라이언트 컴포넌트 하위에 서버 컴포넌트 배치

---
+++ src/components/MainPageClient.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import useMoveScroll from "@/hooks/useMoveScroll";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Intro from "./intro/Intro";
+import About from "./about/About";
+import Description from "./description/Description";
+import Enter from "./enter/Enter";
+
+export default function MainPageClient({ children }: { children: any }) {
+  const { element: enterRef, onMoveToElement: moveToEnter } = useMoveScroll();
+
+  useEffect(() => {
+    AOS.init({ duration: 700 });
+  });
+  return (
+    <>
+      <Intro onClickEnter={moveToEnter} />
+      <About />
+      <Description />
+      <Enter enterRef={enterRef} />
+      {children}
+    </>
+  );
+}
