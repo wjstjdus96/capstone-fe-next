43d8d0f8fce00f4e6ef72948c87a1b9ed0f8b6d6
seoyeon <wjstjdus96@naver.com>
2024-02-25 00:32:50 +0900

    feat: 인기 영상 리스트 hover scale transition 구현

--- src/components/popularVideos/VideoItem.tsx
+++ src/components/popularVideos/VideoItem.tsx
@@ -1,7 +1,7 @@
 export default function VideoItem({ item, idx }: { item: any; idx: number }) {
   return (
-    <li className="group w-full">
-      <div className="flex gap-[1rem] py-[1rem] px-[2rem] bg-white rounded-3xl">
+    <li className="group w-3/4">
+      <div className="flex gap-[1rem] py-[1rem] px-[2rem] relative bg-white rounded-3xl group-hover:scale-105 group-hover:border-solid transition-all">
         <h1>#{idx}</h1>
         <h2>{item.title}</h2>
       </div>
@@ -10,16 +10,19 @@ export default function VideoItem({ item, idx }: { item: any; idx: number }) {
           src={item.thumbnails.medium.url}
           className="w-[160px] object-cover"
         />
-        <div>
+        <div className="flex flex-col justify-around">
           <div className="flex">
             <p>channel. {item.channelTitle}</p>
             <p>{item.publishedAt.slice(0, 10)}</p>
           </div>
-          <div className="flex gap-[0.3rem] flex-wrap">
+          <div className="flex gap-[0.3rem] overflow-hidden">
             {item.tags?.slice(0, 10).map((tag: string) => (
               <p>#{tag}</p>
             ))}
           </div>
+          <h2 className=" opacity-0 group-hover:opacity-100 transition-all">
+            분석하기
+          </h2>
         </div>
       </div>
     </li>
