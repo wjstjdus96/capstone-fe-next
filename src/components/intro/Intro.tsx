e5f1bb21c722bdca02ea128a169af0a2c6dd2564
seoyeon <wjstjdus96@naver.com>
2024-02-26 16:53:32 +0900

    fix: 초기화면 scss로 변경 및 디자인 수정

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -1,29 +1,20 @@
 import Image from "next/image";
 import bgImage from "../../assets/bg-analysis.jpg";
 import IntroSubTitle from "./IntroSubTitle";
+import styles from "../../styles/intro.module.scss";
 
 export default function Intro({ onClickEnter }: { onClickEnter: () => void }) {
   return (
-    <div className="h-[100vh] flex">
-      <div className="p-[5rem] bg-black text-white flex flex-col justify-between">
-        <div className="text-4xl">
-          <IntroSubTitle />
-          <div className="overflow-hidden">
-            <div className="text-8xl mt-[2rem] font-bold animate-fontGrow opacity-0">
-              TubeAna
-            </div>
-          </div>
-        </div>
-        <div
-          data-aos="fade-right"
-          data-aos-delay="2700"
-          onClick={onClickEnter}
-          className="text-xl self-end font-semibold hover:font-bold hover:text-[#D83636] cursor-pointer hover:transition-all hover:ease-in hover:duration-700"
-        >
-          바로 분석하러 가기
-        </div>
+    <div className={styles.wrapper}>
+      <div className={styles.titleBox}>
+        <h1 className={styles.subTitle}>
+          유튜브 댓글 분석을 위한 새로운 플랫폼
+        </h1>
+        <h2 className={styles.title}>TubeAna</h2>
       </div>
-      <Image src={bgImage} alt="메인 배경 이미지" />
+      <button className={styles.button} onClick={onClickEnter}>
+        분석하러 가기
+      </button>
     </div>
   );
 }
