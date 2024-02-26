390f6d4d7a2dd9164d70434683ba29bcf73a926e
seoyeon <wjstjdus96@naver.com>
2024-02-26 16:52:58 +0900

    feat: scss 초기설정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "src/styles/_variables.scss"; @import "src/styles/_mixins.scss";`,
   },
 };
 
