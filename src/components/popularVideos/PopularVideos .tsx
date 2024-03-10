f92c61c101a99cbd03988730143291d7aaaf385c
seoyeon <wjstjdus96@naver.com>
2024-03-10 22:52:29 +0900

    test: 프로덕션 환경 client-side exception 에러 원인 탐색

--- src/components/popularVideos/PopularVideos .tsx
+++ src/components/popularVideos/PopularVideos .tsx
@@ -5,7 +5,7 @@ export default function PopularVideos() {
   return (
     <div className={styles.popluarVideosWrapper}>
       <h2 className={styles.title}>인기 동영상을 분석해보세요!</h2>
-      {/* <VideoItemList /> */}
+      <VideoItemList />
     </div>
   );
 }
--- src/components/popularVideos/VideoItemList.tsx
+++ src/components/popularVideos/VideoItemList.tsx
@@ -2,6 +2,7 @@ import VideoItem from "./VideoItem";
 import styles from "../../styles/popularMovies.module.scss";
 
 export const VideoItemList = async () => {
+  console.log("mount");
   const fetchVideos = async () => {
     const res = await fetch(
       "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&regionCode=KR&key=AIzaSyC6yTlfN6VPRPV9VYV9C_VVwQLDvK78sUg",
@@ -16,9 +17,10 @@ export const VideoItemList = async () => {
 
   return (
     <ul className={styles.videoListWrapper}>
-      {data.items.map((item: any, idx: number) => (
-        <VideoItem key={idx} item={item.snippet} idx={idx + 1} />
-      ))}
+      {data &&
+        data.items.map((item: any, idx: number) => (
+          <VideoItem key={idx} item={item.snippet} idx={idx + 1} />
+        ))}
     </ul>
   );
 };
