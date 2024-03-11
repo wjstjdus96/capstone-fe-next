d1d5c905fce8f90ec92697a2590d85a43e421ba4
seoyeon <wjstjdus96@naver.com>
2024-03-11 11:52:41 +0900

    fix: dynamic import 도입

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Header from "@/components/Header";
 import MainPageClient from "@/components/MainPageClient";
 import dynamic from "next/dynamic";
-import PopularVideos from "@/components/popularVideos/PopularVideos ";
+// import PopularVideos from "@/components/popularVideos/PopularVideos";
+
+const Popular = dynamic(
+  () => import("../components/popularVideos/PopularVideos")
+);
 
 export default function Home() {
   return (
     <div className="h-full font-notoKR ">
       <MainPageClient />
-      <PopularVideos />
+      <Popular />
     </div>
   );
 }
