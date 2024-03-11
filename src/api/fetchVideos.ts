57dd7c18531d00613723dcc24c1eae90ea711760
seoyeon <wjstjdus96@naver.com>
2024-03-11 10:47:51 +0900

    fix: 데이터 fetching 에러 캐치

---
+++ src/api/fetchVideos.ts
@@ -0,0 +1,15 @@
+export const fetchVideos = async () => {
+  const res = await fetch(
+    "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&regionCode=KR&key=AIzaSyC6yTlfN6VPRPV9VYV9C_VVwQLDvK78sUg",
+    {
+      method: "GET",
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("데이터 패칭 실패");
+  }
+
+  return res.json();
+};
--- src/components/popularVideos/VideoItemList.tsx
+++ src/components/popularVideos/VideoItemList.tsx
@@ -1,18 +1,9 @@
 import VideoItem from "./VideoItem";
 import styles from "../../styles/popularMovies.module.scss";
+import { fetchVideos } from "@/api/fetchVideos";
 
 export const VideoItemList = async () => {
   console.log("mount");
-  const fetchVideos = async () => {
-    const res = await fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&regionCode=KR&key=AIzaSyC6yTlfN6VPRPV9VYV9C_VVwQLDvK78sUg",
-      {
-        method: "GET",
-        cache: "no-store",
-      }
-    );
-    return res.json();
-  };
   const data = await fetchVideos();
 
   return (
